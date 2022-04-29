b0e3d2fba7eb7aba1540e2d68b6e8ae4bc0d5971
BarneyShi <barneyshek@gmail.com>
2022-04-29 01:14:12 -0700

    Finish script version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # stream-cleanser
-A tampermonkey script for Douyu.com to filter trolls' comments.
+Have you ever feel tired seeing trolls plaguing streams and want their comments to disappear forever?\
+Here's a tampermonkey script for Douyu.com to filter trolls' comments.\
+Click on the small `X` next to users' nicknames, and they would disappear from comments and danmus forever.
+
+![img](https://i.imgur.com/f92t6N1.png)
+
+Pros:
+- Douyu.com disables some events, but I found `MutationObserver` is very handy to check if some key elements have been loaded.
+- Use `Event Bubbling` to reduce CPU workloads.
+
+Weakness:
+- Block list is stored in `localStorage`, so it'll disappear if you clear browser data.
--- douyu.js
+++ douyu.js
@@ -9,67 +9,100 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-  'use strict';
-setTimeout(() => {
-// Add click event on danmu
-const comments = document.getElementsByClassName("Barrage-list")[0];
-comments.addEventListener("click", (e) => {
-  e.stopPropagation();
-  if (e.target.className && e.target.className.includes("Barrage-nickName")) {
-    // const parent = e.target.parent();
-    // const span = document.createElement("span");
-    // span.innerText("&#10003;");
-    // parent.prepend(span);
-  }
-})
+(function () {
+  "use strict";
 
-// Select the node that will be observed for mutations
-const targetNode = document.getElementById("js-barrage-list");
-console.log("Found: ", targetNode);
+  const documentObserver = new MutationObserver((mutations) => {
+    if (document.getElementById("js-barrage-list")) {
+      documentObserver.disconnect();
+      onwatch();
+    }
+  });
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: false, childList: true, subtree: false };
+  documentObserver.observe(document, {attributes: false, childList: true, characterData: false, subtree:true})
 
-// Callback function to execute when mutations are observed
-const callback = function (mutationsList, observer) {
-  // Use traditional 'for loops' for IE 11
-  for (const mutation of mutationsList) {
-    mutation.addedNodes.forEach((e) => {
-      const name = findNickname(e);
-      if (localStorage.getItem("douyu-blocklist") && localStorage.getItem("douyu-blocklist").has(name)) {
-        removeLastDanmu();
-        console.log("A danmu has been removed.", name);
-      } else {
-        const span = document.createElement("span");
-        span.innerHTML = "&#10003;";
-        span.style.cssText = "color: green; font-weight: bold; ";
-        e.firstElementChild.prepend(span)
+  function onwatch() {
+    // Add click event on danmu
+    const comments = document.getElementsByClassName("Barrage-list")[0];
+    comments.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if (e.target.className && e.target.className.includes("blockBtn")) {
+        if (!localStorage.getItem("douyu-blocklist")) {
+          const str = [e.target.getAttribute("aria-nickname")];
+          localStorage.setItem("douyu-blocklist", JSON.stringify(str));
+        } else {
+          let arr = new Set([
+            ...JSON.parse(localStorage.getItem("douyu-blocklist")),
+          ]);
+          arr.add(e.target.getAttribute("aria-nickname"));
+          localStorage.setItem("douyu-blocklist", JSON.stringify([...arr]));
+        }
       }
-    })
+    });
+
+    // Select the node that will be observed for mutations
+    const targetNode = document.getElementById("js-barrage-list");
+    console.log("Found: ", targetNode);
+
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: false, childList: true, subtree: false };
+
+    // Callback function to execute when mutations are observed
+    const callback = function (mutationsList, observer) {
+      let set;
+      // Use traditional 'for loops' for IE 11
+      for (const mutation of mutationsList) {
+        mutation.addedNodes.forEach((e) => {
+          const name = findNickname(e);
+          if (localStorage.getItem("douyu-blocklist")) {
+            set = new Set([
+              ...JSON.parse(localStorage.getItem("douyu-blocklist")),
+            ]);
+          }
+          if (localStorage.getItem("douyu-blocklist") && set && set.has(name)) {
+            console.log("Removed a danmu!");
+            removeLastDanmu();
+            removeLastComment();
+            console.log("A danmu has been removed.", name);
+          } else {
+            const span = document.createElement("span");
+            span.innerHTML = "&#10006;";
+            span.className = "blockBtn";
+            span.setAttribute("aria-nickname", name);
+            span.style.cssText = "color:red; font-weight:bold; ";
+            e.firstElementChild.prepend(span);
+          }
+        });
+      }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+
+    // Manually wait for 5s to start the script.
   }
-};
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
-}, 5000);
-
-function findNickname(node) {
-const elements = node.childNodes[1].childNodes;
-for (const e of elements) {
-  if (e.className && e.className.includes("Barrage-nickName")) {
-    const name = e.childNodes[0].nodeValue.trim().replace(/：$/, "")
-    return name;
+  function findNickname(node) {
+    const elements = node.childNodes[1].childNodes;
+    for (const e of elements) {
+      if (e.className && e.className.includes("Barrage-nickName")) {
+        const name = e.childNodes[0].nodeValue.trim().replace(/：$/, "");
+        return name;
+      }
+    }
+    return "";
   }
-}
-return "";
-}
 
-function removeLastDanmu() {
-const danmus = document.getElementsByClassName(" danmu-e7f029")[0];
-danmus.removeChild(danmus.lastChild);
-}
+  function removeLastDanmu() {
+    const danmus = document.getElementsByClassName(" danmu-e7f029")[0];
+    danmus.removeChild(danmus.lastChild);
+  }
+
+  function removeLastComment() {
+    const comments = document.getElementById("js-barrage-list");
+    comments.removeChild(comments.lastChild);
+  }
 })();
